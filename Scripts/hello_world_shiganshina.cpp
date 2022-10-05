df47389dfa83d179d6ea50652b9be55efdfd8e36
Om Parulekar <65206237+Shiganshina@users.noreply.github.com>
2022-10-05 22:42:53 +0530

    Create hello_world_shiganshina.cpp

---
+++ Scripts/hello_world_shiganshina.cpp
@@ -0,0 +1,20 @@
+//author : elvenblade
+//जय हिंद, जय महाराष्ट्र !
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define fastio  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define vsort sort(v.begin(),v.end())
+#define vpb v.push_back
+#define asort sort(a,a+n)
+#define vv vector<int> v
+#define rev reverse(a, a+n) 
+
+signed main(){
+    fastio;
+    cout<<"जगाला माझा नमस्कार";
+    return 0;
+}
+
+
