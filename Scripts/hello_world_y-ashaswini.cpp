6e34c85c242b8a0b36d81fbcb2a32f79b0714fe4
Yashaswini Shivathaya <evilgeniusprevails@gmail.com>
2022-10-06 21:09:44 +0530

    left my mark!

---
+++ Scripts/hello_world_y-ashaswini.cpp
@@ -0,0 +1,11 @@
+// Programming Language: C++
+// Native Language: Hindi
+// Author: Yashaswini Shivathaya
+// Github: https://github.com/y-ashaswini
+
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Pranam, Vishv!" << endl;
+    return 0;
+}
