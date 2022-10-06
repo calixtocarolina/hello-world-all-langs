222e15c1e33e075523d5f4e8919575fec47dcc04
Pablo Gabriel <79858234+Poveii@users.noreply.github.com>
2022-10-06 07:13:11 -0300

    Created hello_world_poveii.js

---
+++ Scripts/hello_world_poveii.js
@@ -0,0 +1,6 @@
+// Programming Language: JavaScript
+// Native Language: Portuguese
+// Author: Pablo Gabriel (Poveii)
+// Github: https://github.com/Poveii
+
+document.body.innerHTML = "<div align='center'>" + "<h1>Olá, mundo!</h1>" + "</div>";
