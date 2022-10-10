755d464117dd4a6c02932283225b62c3e904b009
Wendell Borges <62032363+Wendell00@users.noreply.github.com>
2022-10-10 19:33:43 -0300

    Update hello_world_wendell00.ts

--- Scripts/hello_world_wendell00.ts
+++ Scripts/hello_world_wendell00.ts
@@ -1,2 +1,7 @@
+// Programming Language: JavaScript
+// Native Language: Portuguese
+// Author: Wendell Borges
+// Github: https://github.com/wendell00
+
 let msg: string = 'Hello, World!';
 console.log(msg);
